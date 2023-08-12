38e5d747e72cbff225d4170122d1f7b0622ca0b7
maksim <6234513@gmail.com>
2023-08-12 18:05:36 +0300

    create and setup config.py

---
+++ project/config.py
@@ -0,0 +1,36 @@
+import os
+import pathlib
+from functools import lru_cache
+
+
+class BaseConfig:
+    BASE_DIR: pathlib.Path = pathlib.Path(__file__).parent.parent
+    DATABASE_URL: str = os.getenv('DATABASE_URL')
+    DATABASE_CONNECT_DICT: dict = {}
+
+
+class DevelopmentConfig(BaseConfig):
+    pass
+
+
+class ProductionConfig(BaseConfig):
+    pass
+
+
+class TestConfig(BaseConfig):
+    pass
+
+
+@lru_cache()
+def get_settings() -> DevelopmentConfig | ProductionConfig | TestConfig:
+    config_cls_dict = {
+        'development': DevelopmentConfig,
+        'production': ProductionConfig,
+        'testing': TestConfig
+    }
+    config_name = os.getenv('FASTAPI_CONFIG', default='development')
+    config_cls = config_cls_dict[config_name]
+    return config_cls()
+
+
+setting = get_settings()
